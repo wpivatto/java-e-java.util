49a862f71107791e22cc4ec1a71cb462a567919d
Wellington Pivatto <pivatto@live.com>
2022-06-25 09:21:54 -0300

    Criação da classe teste, utilização de array com laço for

--- src/br/com/bytebank/banco/test/Teste.java
+++ src/br/com/bytebank/banco/test/Teste.java
@@ -1,33 +1,43 @@
 package br.com.bytebank.banco.test;
 
-import br.com.bytebank.banco.modelo.Cliente;
-import br.com.bytebank.banco.modelo.Conta;
-import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.ContaPoupanca;
-
 public class Teste {
 
-	public static void main(String[] args) {
 
-//		System.out.println("x");
-//		System.out.println(3);
-//		System.out.println(false);
-		
-		Object cc = new ContaCorrente(22, 33);
-		Object cp = new ContaPoupanca(33, 22);
-		Object cliente = new Cliente();
-		
-		System.out.println(cc);
-		System.out.println(cp);
-		
-		//println(cliente);
-	}
-	
-	static void println() {}
-	static void println(int a) {}
-	static void println(boolean valor) {}
-	
-	static void println(Object referencia) {
-	}
+    //Array
+    public static void main(String[] args) {
+
+
+
+
+        int [] idades = new int[5];
+
+        for( int i = 0; i < idades.length; i++){
+            idades[i]  = i * i;
+
+        }
+
+
+        for( int i = 0; i < idades.length; i++){
+            System.out.println(idades[i]);
+
+        }
+
+
+
+//        idades [0] = 29;
+//        idades [1] = 20;
+//        idades [2] = 21;
+//        idades [3] = 22;
+//        idades [4] = 23;
+//
+//        int indade1 = idades [4];
+//
+//        System.out.println(indade1);
+//        System.out.println(idades.length);
+
+    }
+
+
+
 
 }
